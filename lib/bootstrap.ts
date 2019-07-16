5f13a23ec62b0b3fc9a03c3ef72e175e7389b281
liangshen001 <wangliang520wl@hotmail.com>
2019-07-16 08:33:44 +0800

    发布时去掉ts文件

--- lib/bootstrap.ts
+++ lib/bootstrap.ts
@@ -4,8 +4,8 @@
 import {ExpressResponse} from "./express-response";
 import {ExpressRequest} from "./express-request";
 import * as express from 'express';
-import {Constructor} from "@node-mvc-decorator/core/lib/beans/constructor";
-import {resolveRouter} from "@node-mvc-decorator/core/lib/core";
+import {Constructor} from "@node-mvc-decorator/core";
+import {resolveRouter} from "@node-mvc-decorator/core";
 
 
 export function bootstrap(...constructors: Array<Constructor>): express.Express {
